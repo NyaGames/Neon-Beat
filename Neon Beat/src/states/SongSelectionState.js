f20faf053201db8a0849cf9abedfc9a735a3e476
NyaGames <43405226+NyaGames@users.noreply.github.com>
2019-10-16 16:41:34 +0200

    song selection movidas

--- Neon Beat/src/states/SongSelectionState.js
+++ Neon Beat/src/states/SongSelectionState.js
@@ -12,6 +12,16 @@ function SongSelectionState(){
     var normalButton;
     var hardButton;
 
+    var twilight_button;
+    var twilight_encendido_img;
+    var twilight_apagado_img;
+    var lsd_button;
+    var lsd_encendido_img;
+    var lsd_apagado_img;
+    var orange_button;
+    var orange_encendido_img;
+    var orange_apagado_img;
+
     var selectionImage;  
 
     var songSelectionBGIndex = 0;
@@ -57,10 +67,35 @@ function SongSelectionState(){
         selectionButton.parent(container);
         selectionButton.position(0*wPercentaje, 250*hPercentaje);
         selectionImage = createImg('assets/images/PantallaVictoria/caja_cancion.png');
-        selectionButton.position(0, 0);
         selectionImage.parent(selectionButton);
         selectionImage.size(584*wPercentaje, 278*hPercentaje);
         state = 0;
+
+        //canciones ya metidas
+        twilight_button = createDiv();
+        twilight_button.parent(container);
+        twilight_button.mousePressed(this.loadSong1); 
+        twilight_button.position((1120-824)*wPercentaje, 250*hPercentaje);
+        twilight_encendido_img = createImg('assets/images/seleccionCancion/twilight_encendido.png');
+        twilight_encendido_img.size(824*wPercentaje, 205*hPercentaje);
+        twilight_encendido_img.parent(twilight_button);
+
+        lsd_button = createDiv();
+        lsd_button.parent(container);
+        lsd_button.mousePressed(this.loadSong2); 
+        lsd_button.position((1120-661)*wPercentaje, 60*hPercentaje);
+        lsd_apagado_img = createImg('assets/images/seleccionCancion/lsd_apagado.png');
+        lsd_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+        lsd_apagado_img.parent(lsd_button);
+
+        orange_button = createDiv();
+        orange_button.parent(container);
+        orange_button.mousePressed(this.loadSong3); 
+        orange_button.position((1120-661)*wPercentaje, 440*hPercentaje);
+        orange_apagado_img = createImg('assets/images/seleccionCancion/orange_apagado.png');
+        orange_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+        orange_apagado_img.parent(orange_button);
+
 
         chosenDifficulty = difficulties.normal; 
 
@@ -126,34 +161,197 @@ function SongSelectionState(){
          this.setSize();
     }
     
-    this.loadSongFromURL = function(url){
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.responseType = 'blob';
-        request.onload = function() {
-            var reader = new FileReader();
-            reader.readAsArrayBuffer(request.response);
-            reader.onload =  function(file){
-                songFile = file;
-                canvas.remove();
-                selectionButton.remove();
-                cancion_menu.stop();
-                mgr.showScene(PreloadState);
-            };
-        };
-        request.send(); 
-    }
+   
 
     //De Pablo para Jusi: Con todo el amor del mundo
     this.loadSong1 = function(){
-        this.loadSongFromURL("assets/ost/Kate_Orange_-_Twilight__instrumental_.mp3");
+        if(state === 0){
+            loadSongFromURL("assets/ost/Kate_Orange_-_Twilight__instrumental_.mp3");
+        }else{
+            if(state === 1){
+                lsd_encendido_img.remove();
+                twilight_apagado_img.remove();
+
+                twilight_button.position((1120-824)*wPercentaje, 250*hPercentaje);
+                twilight_encendido_img = createImg('assets/images/seleccionCancion/twilight_encendido.png');
+                twilight_encendido_img.size(824*wPercentaje, 205*hPercentaje);
+                twilight_encendido_img.parent(twilight_button);
+    
+                lsd_button.position((1120-661)*wPercentaje, 60*hPercentaje);
+                lsd_apagado_img = createImg('assets/images/seleccionCancion/lsd_apagado.png');
+                lsd_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+                lsd_apagado_img.parent(lsd_button);
+            }else{
+                orange_encendido_img.remove();
+                twilight_apagado_img.remove();
+
+                twilight_button.position((1120-824)*wPercentaje, 250*hPercentaje);
+                twilight_encendido_img = createImg('assets/images/seleccionCancion/twilight_encendido.png');
+                twilight_encendido_img.size(824*wPercentaje, 205*hPercentaje);
+                twilight_encendido_img.parent(twilight_button);
+    
+                orange_button.position((1120-661)*wPercentaje, 440*hPercentaje);
+                orange_apagado_img = createImg('assets/images/seleccionCancion/orange_apagado.png');
+                orange_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+                orange_apagado_img.parent(orange_button);
+            }
+            state = 0;
+        }
     }
 
     this.loadSong2 = function(){
-        this.loadSongFromURL("assets/ost/Kate_Orange_-_LSD__instrumental_.mp3");
+        if(state === 1){
+            loadSongFromURL("assets/ost/Kate_Orange_-_LSD__instrumental_.mp3");
+        }else{
+            if(state === 0){
+                twilight_encendido_img.remove();
+                lsd_apagado_img.remove();
+                
+                twilight_button.position((1120-661)*wPercentaje, 250*hPercentaje);
+                twilight_apagado_img = createImg('assets/images/seleccionCancion/twilight_apagado.png');
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                twilight_apagado_img.parent(twilight_button);
+    
+                lsd_button.position((1120-824)*wPercentaje, 60*hPercentaje);
+                lsd_encendido_img = createImg('assets/images/seleccionCancion/lsd_encendido.png');
+                lsd_encendido_img.size(824*wPercentaje, 203*hPercentaje);
+                lsd_encendido_img.parent(lsd_button);
+            }else{
+                lsd_apagado_img.remove();
+                orange_encendido_img.remove();
+
+                lsd_button.position((1120-824)*wPercentaje, 60*hPercentaje);
+                lsd_encendido_img = createImg('assets/images/seleccionCancion/lsd_encendido.png');
+                lsd_encendido_img.size(824*wPercentaje, 203*hPercentaje);
+                lsd_encendido_img.parent(lsd_button);
+        
+                orange_button.position((1120-661)*wPercentaje, 440*hPercentaje);
+                orange_apagado_img = createImg('assets/images/seleccionCancion/orange_apagado.png');
+                orange_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+                orange_apagado_img.parent(orange_button);                
+            }
+            state = 1;
+        }
     }
 
     this.loadSong3 = function(){
-        this.loadSongFromURL("assets/ost/Kate_Orange_-_Stereo_radio.mp3");
+        if(state === 2){
+            loadSongFromURL("assets/ost/Kate_Orange_-_Stereo_radio.mp3");
+        }else{
+            if(state === 0){
+                twilight_encendido_img.remove();
+                orange_apagado_img.remove();
+
+                twilight_button.position((1120-661)*wPercentaje, 250*hPercentaje);
+                twilight_apagado_img = createImg('assets/images/seleccionCancion/twilight_apagado.png');
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                twilight_apagado_img.parent(twilight_button);
+        
+                orange_button.position((1120-824)*wPercentaje, 440*hPercentaje);
+                orange_encendido_img = createImg('assets/images/seleccionCancion/orange_encendido.png');
+                orange_encendido_img.size(824*wPercentaje, 203*hPercentaje);
+                orange_encendido_img.parent(orange_button);
+            }else{
+                lsd_encendido_img.remove();
+                orange_apagado_img.remove();
+    
+                lsd_button.position((1120-661)*wPercentaje, 60*hPercentaje);
+                lsd_apagado_img = createImg('assets/images/seleccionCancion/lsd_apagado.png');
+                lsd_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+                lsd_apagado_img.parent(lsd_button);
+        
+                orange_button.position((1120-824)*wPercentaje, 440*hPercentaje);
+                orange_encendido_img = createImg('assets/images/seleccionCancion/orange_encendido.png');
+                orange_encendido_img.size(824*wPercentaje, 203*hPercentaje);
+                orange_encendido_img.parent(orange_button);
+            }
+            state = 2;
+        }
+    }    
+    
+    this.setSize = function(){
+        if (!mobileDevice) {
+            ancho = window.innerWidth - window.innerWidth*0.208*2;   
+            alto = window.innerHeight - window.innerHeight*0.163*2;
+            wPercentaje = ancho/1120;
+            hPercentaje = alto/630;
+            resizeCanvas(ancho, alto);
+            background(0);
+            
+            if(state === 0){
+                twilight_button.position((1120-824)*wPercentaje, 250*hPercentaje);
+                twilight_encendido_img.size(824*wPercentaje, 205*hPercentaje);
+                lsd_button.position((1120-661)*wPercentaje, 60*hPercentaje);
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                orange_button.position((1120-661)*wPercentaje, 440*hPercentaje);
+                orange_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+            }else if(state === 1){
+                twilight_button.position((1120-661)*wPercentaje, 250*hPercentaje);
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                lsd_button.position((1120-824)*wPercentaje, 60*hPercentaje);
+                twilight_encendido_img.size(824*wPercentaje, 205*hPercentaje);
+                orange_button.position((1120-661)*wPercentaje, 440*hPercentaje);
+                orange_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+            }else{                
+                twilight_button.position((1120-661)*wPercentaje, 250*hPercentaje);
+                twilight_encendido_img.size(661*wPercentaje, 205*hPercentaje);
+                lsd_button.position((1120-661)*wPercentaje, 60*hPercentaje);
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                orange_button.position((1120-824)*wPercentaje, 440*hPercentaje);
+                orange_encendido_img.size(824*wPercentaje, 203*hPercentaje);
+            }
+        }else{            
+            ancho = window.innerWidth;
+            alto = window.innerHeight;
+            wPercentaje = ancho/1120;
+            hPercentaje = alto/630;
+            resizeCanvas(ancho, alto);
+            canvas.background(0);           
+            
+            if(state === 0){
+                twilight_button.position((1120-824)*wPercentaje, 250*hPercentaje);
+                twilight_encendido_img.size(824*wPercentaje, 205*hPercentaje);
+                lsd_button.position((1120-661)*wPercentaje, 60*hPercentaje);
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                orange_button.position((1120-661)*wPercentaje, 440*hPercentaje);
+                orange_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+            }else if(state === 1){
+                twilight_button.position((1120-661)*wPercentaje, 250*hPercentaje);
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                lsd_button.position((1120-824)*wPercentaje, 60*hPercentaje);
+                twilight_encendido_img.size(824*wPercentaje, 205*hPercentaje);
+                orange_button.position((1120-661)*wPercentaje, 440*hPercentaje);
+                orange_apagado_img.size(661*wPercentaje, 203*hPercentaje);
+            }else{                
+                twilight_button.position((1120-661)*wPercentaje, 250*hPercentaje);
+                twilight_encendido_img.size(661*wPercentaje, 205*hPercentaje);
+                lsd_button.position((1120-661)*wPercentaje, 60*hPercentaje);
+                twilight_apagado_img.size(661*wPercentaje, 205*hPercentaje);
+                orange_button.position((1120-824)*wPercentaje, 440*hPercentaje);
+                orange_encendido_img.size(824*wPercentaje, 203*hPercentaje);
+            }
+        }
     }
+
+    this.windowResized = function(){
+         this.setSize();
+    }
+}
+
+function loadSongFromURL(url){
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'blob';
+    request.onload = function() {
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(request.response);
+        reader.onload =  function(file){
+            songFile = file;
+            canvas.remove();
+            selectionButton.remove();
+            cancion_menu.stop();
+            mgr.showScene(PreloadState);
+        };
+    };
+    request.send(); 
 }
